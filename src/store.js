2e8dd475c20ea98c8d4dbf4faa7b328ea0b3bfe3
Pratik Gawand <gawand.p@northeastern.edu>
2022-02-18 00:48:22 -0500

    Workflow

--- src/store.js
+++ src/store.js
@@ -1,4 +1,4 @@
-import { configure, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 import jobReducer from "./features/job/jobSlice";
 
 export const store = configureStore({
